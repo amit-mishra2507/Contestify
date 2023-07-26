c8e63c2045875709b1101f1471bac6658d1ef3d8
Amit Mishra <amitsneh2507@gmail.com>
2023-07-26 15:32:10 +0530

    About

--- src/components/About.js
+++ src/components/About.js
@@ -5,7 +5,7 @@ const About = () => {
     <div className="about">
       <h3 className="h3 about-title">Why Contestify</h3>
       <p className="p1">
-       Contestify is a place to get all details about your favo
+      "Contestify: Unleash Your Competitive Edge! Your gateway to a world of thrilling contests and challenges awaits at Contestify! Seamlessly bringing together a diverse range of competitions, from coding and design to writing and sports, we empower individuals to showcase their talents, push their boundaries, and ignite their passion for success. Join our vibrant community, connect with like-minded enthusiasts, and embark on an exhilarating journey of growth, recognition, and achievement. Don't miss out on the opportunity to elevate your competitive spirit – sign up with Contestify today and unleash the champion within!"
       </p>
     </div>
   );
